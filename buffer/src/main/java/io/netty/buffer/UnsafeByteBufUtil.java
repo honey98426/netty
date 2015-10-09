1c118a2f9d38539b9588a2ac057d5b9569195d31
Norman Maurer <norman_maurer@apple.com>
2015-10-09 21:03:03 +0200

    Add *UnsafeHeapByteBuf for improve performance on systems with sun.misc.Unsafe
    Motivation:
    
    sun.misc.Unsafe allows us to handle heap ByteBuf in a more efficient matter. We should use special ByteBuf implementation when sun.misc.Unsafe can be used to increase performance.
    
    Modifications:
    
    - Add PooledUnsafeHeapByteBuf and UnpooledUnsafeHeapByteBuf that are used when sun.misc.Unsafe is ready to use.
    - Add UnsafeHeapSwappedByteBuf
    
    Result:
    
    Better performance when using heap buffers and sun.misc.Unsafe is ready to use.

--- buffer/src/main/java/io/netty/buffer/UnsafeByteBufUtil.java
+++ buffer/src/main/java/io/netty/buffer/UnsafeByteBufUtil.java
@@ -116,5 +116,94 @@ final class UnsafeByteBufUtil {
         }
     }
 
+    static byte getByte(byte[] array, int index) {
+        return PlatformDependent.getByte(array, index);
+    }
+
+    static short getShort(byte[] array, int index) {
+        if (UNALIGNED) {
+            short v = PlatformDependent.getShort(array, index);
+            return NATIVE_ORDER ? v : Short.reverseBytes(v);
+        }
+        return (short) (PlatformDependent.getByte(index) << 8 | PlatformDependent.getByte(index + 1) & 0xff);
+    }
+
+    static int getUnsignedMedium(byte[] array, int index) {
+        return  (PlatformDependent.getByte(array, index) & 0xff) << 16 |
+                (PlatformDependent.getByte(array, index + 1) & 0xff) <<  8 |
+                PlatformDependent.getByte(array, index + 2) & 0xff;
+    }
+
+    static int getInt(byte[] array, int index) {
+        if (UNALIGNED) {
+            int v = PlatformDependent.getInt(array, index);
+            return NATIVE_ORDER ? v : Integer.reverseBytes(v);
+        }
+        return PlatformDependent.getByte(array, index) << 24 |
+                (PlatformDependent.getByte(array, index + 1) & 0xff) << 16 |
+                (PlatformDependent.getByte(array, index + 2) & 0xff) <<  8 |
+                PlatformDependent.getByte(array, index + 3) & 0xff;
+    }
+
+    static long getLong(byte[] array, int index) {
+        if (UNALIGNED) {
+            long v =  PlatformDependent.getLong(array, index);
+            return NATIVE_ORDER ? v : Long.reverseBytes(v);
+        }
+        return (long) PlatformDependent.getByte(array, index) << 56 |
+                ((long) PlatformDependent.getByte(array, index + 1) & 0xff) << 48 |
+                ((long) PlatformDependent.getByte(array, index + 2) & 0xff) << 40 |
+                ((long) PlatformDependent.getByte(array, index + 3) & 0xff) << 32 |
+                ((long) PlatformDependent.getByte(array, index + 4) & 0xff) << 24 |
+                ((long) PlatformDependent.getByte(array, index + 5) & 0xff) << 16 |
+                ((long) PlatformDependent.getByte(array, index + 6) & 0xff) <<  8 |
+                (long) PlatformDependent.getByte(array, index + 7) & 0xff;
+    }
+
+    static void setByte(byte[] array, int index, int value) {
+        PlatformDependent.putByte(array, index, (byte) value);
+    }
+
+    static void setShort(byte[] array, int index, int value) {
+        if (UNALIGNED) {
+            PlatformDependent.putShort(array, index, NATIVE_ORDER ? (short) value : Short.reverseBytes((short) value));
+        } else {
+            PlatformDependent.putByte(array, index, (byte) (value >>> 8));
+            PlatformDependent.putByte(array, index + 1, (byte) value);
+        }
+    }
+
+    static void setMedium(byte[] array, int index, int   value) {
+        PlatformDependent.putByte(array, index, (byte) (value >>> 16));
+        PlatformDependent.putByte(array, index + 1, (byte) (value >>> 8));
+        PlatformDependent.putByte(array, index + 2, (byte) value);
+    }
+
+    static void setInt(byte[] array, int index, int   value) {
+        if (UNALIGNED) {
+            PlatformDependent.putInt(array, index, NATIVE_ORDER ? value : Integer.reverseBytes(value));
+        } else {
+            PlatformDependent.putByte(array, index, (byte) (value >>> 24));
+            PlatformDependent.putByte(array, index + 1, (byte) (value >>> 16));
+            PlatformDependent.putByte(array, index + 2, (byte) (value >>> 8));
+            PlatformDependent.putByte(array, index + 3, (byte) value);
+        }
+    }
+
+    static void setLong(byte[] array, int index, long  value) {
+        if (UNALIGNED) {
+            PlatformDependent.putLong(array, index, NATIVE_ORDER ? value : Long.reverseBytes(value));
+        } else {
+            PlatformDependent.putByte(array, index, (byte) (value >>> 56));
+            PlatformDependent.putByte(array, index + 1, (byte) (value >>> 48));
+            PlatformDependent.putByte(array, index + 2, (byte) (value >>> 40));
+            PlatformDependent.putByte(array, index + 3, (byte) (value >>> 32));
+            PlatformDependent.putByte(array, index + 4, (byte) (value >>> 24));
+            PlatformDependent.putByte(array, index + 5, (byte) (value >>> 16));
+            PlatformDependent.putByte(array, index + 6, (byte) (value >>> 8));
+            PlatformDependent.putByte(array, index + 7, (byte) value);
+        }
+    }
+
     private UnsafeByteBufUtil() { }
 }
